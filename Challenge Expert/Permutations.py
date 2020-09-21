67df54104d2cb31da6d8b833873811c246c6cdf7
Loc Lam <shenlong3010@gmail.com>
2020-09-21 09:11:22 -0400

    change file paths and directories

--- Challenge Expert/Permutations.py
+++
@@ -1,6 +0,0 @@
-def getPermutations(array):
-    index = 0
-    if len(array) == 0:
-		return []
-	if len(array) > 0:
-        return getPermutations()
---
+++ challenge-expert/Permutations.py
@@ -0,0 +1,6 @@
+def getPermutations(array):
+    index = 0
+    if len(array) == 0:
+		return []
+	if len(array) > 0:
+        return getPermutations()
